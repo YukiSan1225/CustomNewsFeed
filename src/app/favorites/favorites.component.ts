02d14476d1ef427c2bc605c6eaa21208c23d22b8
Damien Burks <dburksgtr@gmail.com>
2019-07-11 23:27:42 -0500

    First Commit

---
+++ src/app/favorites/favorites.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites',
+  template: `
+    <p>
+      favorites works!
+    </p>
+  `,
+  styles: []
+})
+export class FavoritesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiKey: '4a5a55d0c9344004857c11f481fe5727',
+  apiUrl: 'https://newsapi.org/v2'
+};
